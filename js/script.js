d006fc8133fa58c42707aedfe76c1f7bdc915fa3
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-25 16:47:54 +0200

    aggiunto timer e scritta dopo 30 sec

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const minimum = 1;
 const maximum = 10;
 const randomNumber = [];
 const numberContainer = document.getElementById('numbers-container');
+const timeToWait = 30;
 
 for (let i = 0; i < 5; i++) {
     const num = getRandomNumber(minimum, maximum);
@@ -10,12 +11,22 @@ for (let i = 0; i < 5; i++) {
     randomNumber.push(num);
     numberContainer.innerHTML += `
     <span>
-    ${num}
+        ${num}
     </span>
     `;
 }
 
 console.log('randomNumber', randomNumber, typeof randomNumber);
+
+setTimeout(function () {
+    console.log('Tempo scaduto');
+
+    numberContainer.innerHTML = `
+    <h1>
+        Inserisci i numeri in base a come li hai visti in precedenza
+    </h1>
+    `;
+}, timeToWait * 1000);
 
 // FUNZIONI
 function getRandomNumber(min, max) {
