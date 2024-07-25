f43919148e2f81a5cae1a7ed0bf2d6141d3d82f4
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-25 17:09:53 +0200

    finito esercizio

--- js/script.js
+++ js/script.js
@@ -19,13 +19,29 @@ for (let i = 0; i < 5; i++) {
 console.log('randomNumber', randomNumber, typeof randomNumber);
 
 setTimeout(function () {
-    console.log('Tempo scaduto');
-
     numberContainer.innerHTML = `
     <h1>
         Inserisci i numeri in base a come li hai visti in precedenza
     </h1>
     `;
+}, (timeToWait - 1) * 1000);
+
+setTimeout(function () {
+    console.log('Tempo scaduto');
+
+    let guessedNumber = 0;
+    const guessedNumberList = [];
+    for (let i = 0; i <  randomNumber.length; i++) {
+        const userNumber = parseInt(prompt(`Inserisci il ${i + 1}° numero:`));
+        console.log('userNumber', userNumber, typeof userNumber);
+
+        if (userNumber == randomNumber[i]) {
+            guessedNumber++
+            guessedNumberList.push(userNumber);
+        }
+    }
+
+    alert(`hai indovinato ${guessedNumber} numeri! Hai indovinato i seguenti numeri: ${guessedNumberList.join(', ')}`)
 }, timeToWait * 1000);
 
 // FUNZIONI
